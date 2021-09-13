7294c1336aa6851e30e0292cc96b41f748e1f7b7
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-13 13:56:13 +0900

    rails_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'sessions#new'
+  root 'sessions#new'
   
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
